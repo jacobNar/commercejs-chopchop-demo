e2c47c844780ea574894f0fae64631747b32adf0
snorresovold <73354016+snorresovold@users.noreply.github.com>
2021-05-19 22:59:45 +0200

    made kategorier page

--- components/Header.js
+++ components/Header.js
@@ -7,7 +7,11 @@ function Header() {
     <header className="md:absolute md:left-0 md:top-0 w-full z-10">
       <div className="py-3 lg:py-5 flex items-center">
         <Link href="/">
-          <a title="Return to ChopChop">Butikk</a>
+          <a title="Return to store">Butikk</a>
+        </Link>
+        <span className="pr-1">,</span>
+        <Link href="/kategorier">
+          <a title="Til kategorisiden">Kategorier</a>
         </Link>
         <span className="pr-1">,</span>
         <CartSummary />
