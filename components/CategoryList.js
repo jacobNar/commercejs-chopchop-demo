1ac429c157a4af738ad2c4bae710c7105322380d
snorresovold <73354016+snorresovold@users.noreply.github.com>
2021-06-07 22:21:02 +0200

    updates

--- components/CategoryList.js
+++ components/CategoryList.js
@@ -3,12 +3,15 @@ import Link from "next/link";
 function CategoryList({ categories }) {
 
   return categories.map(({ name, slug }, index) => (
-    <span key={slug}>
-      {index ? ", " : ""}
+    <ul>
+          <li key={slug}>
+      {index ? " " : ""}
       <Link href={`/kategorier/${slug}`}>
         <a className="text-lg md:text-xl lg:text-2xl hover:italic">{name}</a>
       </Link>
-    </span>
+    </li>
+    </ul>
+
   ));
 }
 
