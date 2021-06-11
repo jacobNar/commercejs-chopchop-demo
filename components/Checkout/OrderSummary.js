c7cecd96f36513b2d2b0f62eec6fa7c41499853d
snorresovold <73354016+snorresovold@users.noreply.github.com>
2021-06-11 15:06:08 +0200

    updates

--- components/Checkout/OrderSummary.js
+++ components/Checkout/OrderSummary.js
@@ -10,10 +10,10 @@ function CheckoutSummary({ has, fulfillment, order }) {
       <div className="md:flex md:justify-between md:space-x-6">
         <div className="w-full md:w-1/2">
           <ol>
-            <li>Subtotal: {subtotal.formatted_with_symbol}</li>
-            {tax && <li>Tax: {tax.amount.formatted_with_symbol}</li>}
+            <li>TotalPris: {subtotal.formatted_with_symbol}</li>
+            {tax && <li>Skatt: {tax.amount.formatted_with_symbol}</li>}
             {shipping && (
-              <li>Shipping: {shipping.price.formatted_with_symbol}</li>
+              <li>Frakt: {shipping.price.formatted_with_symbol}</li>
             )}
             {total && (
               <li className="text-lg md:text-xl py-3">
--- components/Checkout/Success.js
+++ components/Checkout/Success.js
@@ -4,34 +4,21 @@ function Success({ has }) {
   return (
     <div className="h-full lg:flex lg:items-center lg:space-x-12 lg:space-x-24">
       <div className="lg:w-1/2 ">
-        <h1 className="font-serif font-medium italic text-2xl md:text-4xl lg:text-5xl xl:text-6xl">
-          Thanks!
+        <h1 className=" text-2xl md:text-4xl lg:text-5xl xl:text-6xl">
+          Takk!
         </h1>
         <p className="mt-3 text-lg md:text-xl font-sans">
           {has.digital_fulfillment
-            ? "You’ll receive an email with your receipt, and a backup link to re-download your purchase"
-            : "You’ll receive an email with your receipt, and tracking information."}
+            ? "Du vil få en E-mail med kvittering og en link som lar deg laste ned kvitteringen"
+            : "Du vil få en E-amil med kvittering og frakt informasjon"}
         </p>
       </div>
       <div className="lg:w-1/2 lg:flex lg:items-center lg:justify-center">
         <div className="bg-white shadow-thank-you transform -rotate-25 skew-y-12 mx-auto my-24 lg:mt-48 max-w-lg">
           <div className="ml-4">
-            <Image
-              src="/checkout/doesntexist.svg"
-              width={384}
-              height={126}
-              alt="ChopChop doesn't exist!"
-              layout="responsive"
-            />
           </div>
             <div className="mt-6 mb-1 font-serif flex justify-between items-end">
-              <Image
-                src="/product-attributes/thanks.svg"
-                width={110}
-                height={48}
-                alt="Thanks for visiting"
-              />
-              <span className="ml-4 italic text-sm">'Chop chop' what are you waiting for</span>
+              <span className="ml-4 italic text-sm">Takk for at du handler med CaravanOutlet</span>
             </div>
           </div>
         </div>
